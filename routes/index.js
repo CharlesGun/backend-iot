f5787bea37d91af95d0901d8b74b690adc9dcc54
Charles <charlesgunawan32@gmail.com>
2022-12-01 22:34:37 +0700

    add route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const c = require('../controllers')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'IOT-API' });
 });
+router.post('/iot/sensor', c.value);
+router.get('/iot/sensor', c.getAll);
 
+module.exports = router;
 module.exports = router;
